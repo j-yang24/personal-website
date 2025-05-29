7d377741c79d5f5561247bfee2f3732e78c4a33a
jyang-24 <johnsony326@gmail.com>
2025-05-29 16:08:09 -0400

    update Now Page

--- src/pages/now.js
+++ src/pages/now.js
@@ -3,30 +3,36 @@ import Layout from '../components/layout'
 import Seo from '../components/seo'
 import IconLinks from '../components/IconLinks';
 import BlackBarDivider from '../components/BlackBarDivider'
+import * as styles from '../styles/now.module.css';
 
 const NowPage = () => {
   return (
     <Layout pageTitle showTitle={false}>
-      
       <div>
         <h1>Now</h1>
-        <p>I'm currently living in Pittsburgh, working at <a href="https://www.mbakerintl.com" target="_blank" rel="noopener noreferrer">Michael Baker International</a> as a Bridge Software Systems Developer.
+        <p className={styles.paragraph}>
+          I'm currently living in Pittsburgh, working at{' '}
+          <a className={styles.link} href="https://www.mbakerintl.com" target="_blank" rel="noopener noreferrer">
+            Michael Baker International
+          </a>{' '}
+          as a Bridge Software Systems Developer.
         </p>
-        <p>I'm also pursuing a Master's Degree in Structural Engineering and Mechanics at the <a href="https://www.engineering.pitt.edu/academics/eagr/" target="_blank" rel="noopener noreferrer">University of Pittsburgh</a>.
+        <p className={styles.paragraph}>
+          I'm also pursuing a Master's Degree in Structural Engineering and Mechanics at the{' '}
+          <a className={styles.link} href="https://www.engineering.pitt.edu/academics/eagr/" target="_blank" rel="noopener noreferrer">
+            University of Pittsburgh
+          </a>.
         </p>
-        <br></br>
-        <p><i>Last updated on May 31st, 2025.</i></p>
+        <br />
+        <p className={styles.update}>Last updated on May 31st, 2025.</p>
       </div>
-      
+
       <BlackBarDivider />
       <IconLinks />
       <footer align="center">&#169; 2025 Johnson Yang</footer>
     </Layout>
-
   )
 }
 
-
 export const Head = () => <Seo title="Now" />
-
 export default NowPage
