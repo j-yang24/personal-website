7855a611364c80004f0d64c443c67c9840b7c784
Johnson Yang <johnsony326@gmail.com>
2025-07-26 09:08:54 -0400

    setup figures and captions + styling changes

--- src/components/Divider.js
+++ src/components/Divider.js
@@ -3,7 +3,7 @@ import React from 'react'
 const TopDivider = () => {
   return (
     <div style={{ margin: '5px 0' }}>
-      <div style={{ height: '4px', backgroundColor: 'rgba(0, 0, 0, 0.1)', marginBottom: '2px' }}></div>
+      <div style={{ height: '8px', backgroundColor: 'rgba(0, 0, 0, 0.1)', marginBottom: '2px' }}></div>
     </div>
   )
 }
--- src/pages/0.js
+++ src/pages/0.js
@@ -4,6 +4,7 @@ import Seo from '../components/seo'
 import Footer from '../components/Footer';
 import Divider from '../components/Divider'
 import * as styles from '../components/bloglayout.module.css'
+import cathyImage from '../images/cathy.jpg'
 
 const BlogPage = () => (
     <BlogLayout pageTitle showTitle={false}>
@@ -14,6 +15,11 @@ const BlogPage = () => (
 
     <p className={styles.paragraph}>My name is Johnson Yang and I’m an Asian born American from two Fuzhounese parents. As a child I grew up in Chinatown, New York all the way till I was in second grade. During that time, I lived with my grandparents and cousins— all six of us crammed into a one-bedroom apartment complex. Looking back, it didn’t really feel cramped at the time, but these days when I go back— everything feels so small in comparison.</p> 
 
+    
+    <figure>
+      <img className={styles.img} src={cathyImage} alt='cathy' />
+      <figcaption className={styles.caption}>This is a test caption</figcaption>
+    </figure>
     <Divider />
     <Footer/>
   </BlogLayout>
